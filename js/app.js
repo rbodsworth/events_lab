d66b3b7de9eefbb6fa0ba7bee0f70e9a48dd1c16
Richard Bodsworth <richardbodsworth@gmail.com>
2021-01-08 15:28:42 +0000

    mvp done

---
+++ js/app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+  const form = document.querySelector('#new-item-form');
+
+  const button = document.createElement('button');
+  button.textContent = "Delete All";
+  button.classList.add('delete');
+  form.appendChild(button);
+
+
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+
+  const newItemTitle = document.createElement('li');
+  const newItemAuthor = document.createElement('li');
+  const newItemCategory = document.createElement('li');
+  const readingListItem = document.createElement('ul');
+  const readingList = document.querySelector('#reading-list');
+
+  newItemTitle.textContent = e.target.title.value;
+  newItemAuthor.textContent = e.target.author.value;
+  newItemCategory.textContent = e.target.category.value;
+  readingListItem.appendChild(newItemTitle);
+  readingListItem.appendChild(newItemAuthor);
+  readingListItem.appendChild(newItemCategory);
+  readingListItem.classList.add('list-item');
+  readingList.appendChild(readingListItem);
+  
+  const form = document.querySelector('#new-item-form')
+  form.reset()
+  
+
+  
+}
